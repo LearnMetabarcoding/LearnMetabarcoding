fe9883355395d7765759ddf58f11323784535432
ljp216 <liampenlington@live.co.uk>
2020-12-09 13:41:23 +0000

    Auto-removal of previous html version

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -31,7 +31,7 @@ extensions = ['sphinx.ext.githubpages','sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
