44adbff36f307d9acfb972acc258be45e0a2e2ad
Thomas J. Creedy <thomas@tjcreedy.co.uk>
2020-12-26 17:31:26 +0000

    Added data zipping commands

--- make_script.sh
+++ make_script.sh
@@ -8,5 +8,10 @@ mv html docs
 touch docs/.nojekyll
 cp sphinx/source/custom.css docs/_static/css
 cp sphinx/source/customjs/custom.js docs/_static/js
+### Thomas added ###
+cd data_source/
+for d in *; do zip -r ../docs/data/${d}.zip $d; done
+cd ../
+####################
 git add docs
 git commit -am "Auto-commit of new html version"
