f1de86eb7805b71302bca9799923dc2bf8005f95
ljp216 <liampenlington@live.co.uk>
2020-12-29 14:04:49 +0000

    Auto-removal of previous html version

--- make_script.sh
+++ make_script.sh
@@ -8,6 +8,9 @@ mv html docs
 touch docs/.nojekyll
 cp sphinx/source/custom.css docs/_static/css
 cp sphinx/source/customjs/custom.js docs/_static/js
+cd docs/
+mkdir data
+cd ../
 ### Thomas added ###
 cd data_source/
 for d in *; do zip -r ../docs/data/${d}.zip $d; done
